2f39fc9b4a4b4fe5a49ee59867c132fade9b6360
Chris Ashton <ChrisBAshton@users.noreply.github.com>
2016-02-15 12:36:03 +0000

    Update iframemanager__host.js

--- source/js/lib/news_special/iframemanager__host.js
+++ source/js/lib/news_special/iframemanager__host.js
@@ -173,8 +173,6 @@
                 this.data.hostPageCallback &&
                 (!this.iframeInstructionsRan)
             ) {
-                /* jshint evil:true */
-                (new Function(this.data.hostPageCallback)());
                 this.iframeInstructionsRan = true;
             }
         },
